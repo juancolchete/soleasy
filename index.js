6d370aa3de72483e0836e969f95ca110cda00a68
juancolchete <juanudkp@gmail.com>
2023-08-05 12:14:36 -0300

    feat: implement basic surya command

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
 console.log("Made Solidity easy");
+(0, child_process_1.exec)(`npx surya parse contract/test.sol --json true`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    let contractData = JSON.parse(stdout);
+    console.log(contractData);
+});
--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
+import { exec } from "child_process";
 
 console.log("Made Solidity easy")
+
+exec(`npx surya parse contract/test.sol --json true`, (error: any, stdout: string, stderr: any) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  let contractData = JSON.parse(stdout);
+  console.log(contractData)
+});
