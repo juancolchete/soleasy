15459fd623e79f8c26cef0e6de6cde61a56b80a2
juancolchete <juanudkp@gmail.com>
2023-11-27 19:30:25 -0300

    feat: new run paradigm to allow params functions

--- index.ts
+++ index.ts
@@ -3,15 +3,25 @@ import { exec } from "child_process";
 
 console.log("Made Solidity easy")
 
-exec(`npx surya parse contract/test.sol --json true`, (error: any, stdout: string, stderr: any) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  let contractData = JSON.parse(stdout);
-  console.log(contractData)
-});
+const cliFunctions:any = {
+  genABI: ()=>{
+    console.log("genABI")
+  },
+  compile: ()=>{
+    exec(`npx surya parse contract/test.sol --json true`, (error: any, stdout: string, stderr: any) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      let contractData = JSON.parse(stdout);
+        console.log(contractData)
+      });
+    }
+}
+
+const cliArgs = process.argv.slice(2)
+cliFunctions[cliArgs[0]]();
