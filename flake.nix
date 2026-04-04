795b6be4d800cf4d0a24711bced006c7752ac641
juancolchete <juanudkp@gmail.com>
2026-04-04 16:47:07 -0300

    chore: configure nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Solidity development made so easy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "soleasy";
+
+        buildInputs = with pkgs; [
+          pnpm
+        ];
+      };
+    };
+}
+
